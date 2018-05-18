c0e8f8dc3f79c9647f3b31940ec17b5220983922
AntonioSAlmeida <asalmeida319@gmail.com>
2018-05-18 12:14:20 +0100

    fixed clipboard_wait

--- library.c
+++ library.c
@@ -8,13 +8,13 @@
 
 
 /********************************************************************************
- * Function called by the application to connect to a distributed clipboard.	*
- *																				*
- *char *clipboard directory - directory where the clipboard was created.		*
- *																				*
- *output - file descriptor of the socket created between the application and	*
- *			the clipboard. 														*
- *																				*
+ * Function called by the application to connect to a distributed clipboard.		*
+ *																																							*
+ * char *clipboard directory - directory where the clipboard was created.				*
+ *																																							*
+ * output - file descriptor of the socket created between the application and		*
+ *					the clipboard. 																											*
+ *																																							*
  ********************************************************************************/
 int clipboard_connect(struct sockaddr_un server_addr){
 
@@ -33,17 +33,18 @@ int clipboard_connect(struct sockaddr_un server_addr){
 }
 
 /********************************************************************************
- * Copies data pointed by buf to a region on the local clipboard.				*
- *																				*
- * char *clipboard directory - directory where the clipboard was created.		*
- *																				*
- * output - size of data copied to buf.											*
- *			0 if error.															*
- *																				*
+ * Copies data pointed by buf to a region on the local clipboard.								*
+ *																																							*
+ * char *clipboard directory - directory where the clipboard was created.				*
+ *																																							*
+ * output - size of data copied to buf.																					*
+ *					0 if error.																													*
+ *																																							*
  ********************************************************************************/
 int clipboard_copy(int clipboard_id, int region, void *buf, size_t count){
 
 	if(region >= 10){
+		perror("invalid region: ");
 		return 0;
 	}
 
@@ -85,14 +86,14 @@ int clipboard_copy(int clipboard_id, int region, void *buf, size_t count){
 }
 
 /********************************************************************************
- * Copies data from a region on the local clipboard to memory pointed by buf.	*
- *																				*
- * char *clipboard directory - directory where the clipboard was created.		*
- *																				*
- * output - file descriptor of the socket created between the application and	*
- *			the clipboard. 														*
- *			0 if error.															*
- *																				*
+ * Copies data from a region on the local clipboard to memory pointed by buf.		*
+ *																																							*
+ * char *clipboard directory - directory where the clipboard was created.				*
+ *																																							*
+ * output - file descriptor of the socket created between the application and		*
+ *					the clipboard. 																											*
+*					0 if error.																														*
+ *																																							*
  ********************************************************************************/
 int clipboard_paste(int clipboard_id, int region, void *buf, size_t count){
 
@@ -104,7 +105,6 @@ int clipboard_paste(int clipboard_id, int region, void *buf, size_t count){
 	msg_struct.order = PASTE;
 	msg_struct.region = region;
 	msg_struct.data_size = 0;
-
 	char *bufstruct=malloc(sizeof(message));
 	int err_msg;
 	char *msg = malloc(sizeof(message));
@@ -139,50 +139,49 @@ int clipboard_paste(int clipboard_id, int region, void *buf, size_t count){
 
 /********************************************************************************
  * Waits for a change in a certain region and, when it happens, copies the new	*
- * data	to memory pointed by buf.												*
- *																				*
- * char *clipboard directory - directory where the clipboard was created.		*
- *																				*
- * output - number of bytes copied to buf.										*
- *			0 if error.															*
- *																				*
+ * data	to memory pointed by buf.																								*
+ *																																							*
+ * char *clipboard directory - directory where the clipboard was created.				*
+ *																																							*
+ * output - number of bytes copied to buf.																			*
+ *					0 if error.																													*
+ *																																							*
  ********************************************************************************/
 int clipboard_wait(int clipboard_id, int region, void *buf, size_t count){
 
-	char *init_clip = (char*)malloc((sizeof(char)*count)+1);
-	char *current_clip = (char*)malloc((sizeof(char)*count));
+	char *init_clip = (char*)malloc((sizeof(char)*count));
 
 	int err = clipboard_paste(clipboard_id, region, init_clip, count);
-
 	if(err == -1){
 		perror("paste: ");
 		return 0;
 	}
 
+	err = clipboard_paste(clipboard_id, region, (char *)buf, count);
+	if(err == -1){
+		perror("paste: ");
+		return 0;
+	}
 
-	while(strcmp(init_clip, current_clip) == 0){
+	while(strcmp(init_clip, buf) == 0){
 		sleep(2); //just to try and not consume too many resources by constantly making comparisons...
-		int err = clipboard_paste(clipboard_id, region, current_clip, count);
-		printf("%s | %s\n", init_clip, current_clip);
+		int err = clipboard_paste(clipboard_id, region, (char *)buf, count);
+		//printf("%s | %s\n", init_clip, (char *)buf);
 		if(err == 0){
 			perror("paste: ");
 			return 0;
 		}
 	}
 
-	buf = malloc((sizeof(char)*count)+1);
-	buf = current_clip;
-
 	free(init_clip);
-	free(current_clip);
 	return count;
 }
 
 /********************************************************************************
- * Closes the connection between the application and the local clipboard.		*
- *																				*
+ * Closes the connection between the application and the local clipboard.				*
+ *																																							*
  * int clipboard_id - file descriptor of the connection to the local clipboard.	*
- *																				*
+ *																																							*
  ********************************************************************************/
 void clipboard_close(int clipboard_id){
 	close(clipboard_id);
