08867e87b2da2c9c7a88830376962f8cf5d36e06
Joao Alves <jmalves5@hotmail.com>
2018-06-01 19:32:06 +0100

    only comments missing

--- app_teste.c
+++ app_teste.c
@@ -9,7 +9,7 @@
 
 #include "clipboard.h"
 
-
+/*Creates 8 threads that randomly write the user inputs into clipboard regions*/
 int main(){
 
 	struct sockaddr_un server_addr;
@@ -28,7 +28,7 @@ int main(){
 	server_addr.sun_family = AF_UNIX;
 	strcpy(server_addr.sun_path, CLIPBOARD_SOCKET);
 
-	//Create socket
+	//C0nnect
 	sock_fd=clipboard_connect(server_addr);
 
 	char* m = malloc(100*sizeof(char));
@@ -37,35 +37,40 @@ int main(){
 	void* buff = malloc(100*sizeof(char));
 	void* buff2 = malloc(100*sizeof(char));
 
-	//Send to clipboard something
-	printf("Write something to copy to some regions\n-> ");
+	//User input
+	printf("Write something to copy to some regions\n->");
 	fgets(m, 100*sizeof(char), stdin);
 	printf("Write something else to write to some regions\n->");
 	fgets(buff2, 100*sizeof(char), stdin);
 
-	fork();
+	//Creates 8 threads
+	int pid=fork();
 	fork();
 	fork();
 	
-
+	//Randomizes region for which to copy
 	srand(getpid());
-	reg = (rand() % 9);
+	reg = (rand() % 10);
+	//Threads with even PID's write a string, threads with odd PID's write another
 	if(getpid()%2==0)
 		clipboard_copy(sock_fd, reg, m, (strlen(m)+1)*sizeof(char));
 	else
 		clipboard_copy(sock_fd, reg, buff2, (strlen(buff2)+1)*sizeof(char));
 
-	if(getpid()==0){
+
+	if(getpid()==pid){
 		//wait until position 3 changes
 		printf("waiting\n");
 		clipboard_wait(sock_fd, 3, buff, 100);
-		printf("WAITED ALL THIS TIME FOR THIS!?!?\n->%s\n", (char*)buff);
-	
+		printf("WAITED ALL THIS TIME FOR THIS!?!?\n->%s\n", (char*)buff);	
 		close(sock_fd);
+		free(m);
+		free(buff);
+		free(buff2);
 		exit(0);
 	}
-
-	free(m);
-	free(buff);
 	
+	
+	
+
 }
